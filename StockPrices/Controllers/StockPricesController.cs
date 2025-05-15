9d76a76acfa0f5c7c58ace6138c37ae58d55d1c1
Marcin Pałys <marcin.palys@microsoft.wsei.edu.pl>
2025-05-15 11:51:51 +0200

    Adding the getAll method

---
+++ StockPrices/Controllers/StockPricesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockPrices.Infrastructure.Entities;
+
+namespace StockPrices.Controllers
+{
+    [Route("api/StockPrices")]
+    public class StockPricesController : Controller
+    {
+        private readonly StockPricesDbContext dbContext;
+        public StockPricesController(StockPricesDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+        public ActionResult<IEnumerable<StockPrice>> GetAll()
+        {
+            var stockPrices = dbContext.StockPrices.Take(100).ToList();
+            return Ok(stockPrices);
+        }
+    }
+}
