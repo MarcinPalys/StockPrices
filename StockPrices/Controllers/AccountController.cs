b4c2b286c5c2a6c0195d30b3cd338ba6f282d1a6
Marcin Pałys <marcin.palys@microsoft.wsei.edu.pl>
2025-06-07 15:27:58 +0200

    Adding user registration

--- StockPrices/Controllers/AccountController.cs
+++ StockPrices/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StockPrices.Core.Entities;
+using StockPrices.Infrastructure;
 
 namespace StockPrices.Controllers
 {
@@ -7,11 +8,23 @@ namespace StockPrices.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly StockPricesDbContext _stockPricesDbContext;
+        public AccountController(StockPricesDbContext stockPricesDbContext)
+        {
+            _stockPricesDbContext = stockPricesDbContext;
+        }
         [HttpPost("register")]
         public ActionResult RegisterUser([FromBody] UserDto user)
         {
-            
-            return Ok("User registered successfully");
+            var User = new User()
+            {
+                Email = user.Email,
+                Password = user.Password,
+            };
+            _stockPricesDbContext.Users.Add(User);
+            _stockPricesDbContext.SaveChanges();
+
+            return Ok();
         }
     }
 }
